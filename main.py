bd610dfe4142c094c30946121f1db420ce1109cc
Juan <juan.rossi1804@gmail.com>
2023-11-06 18:34:00 -0300

    fixes

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 from fastapi import FastAPI
 from model import order,product,table,waiter
 from config.config import engine
+from routers import productRouter
 
 app=FastAPI()
+app.include_router(productRouter.router)
 
 product.base.metadata.create_all(bind=engine)
 order.base.metadata.create_all(bind=engine)
